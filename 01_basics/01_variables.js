1164146110f9b10ed83f6baaaf7000c52a81ebea
Satyajeet Parmar <65767924+satyajeetparmar-UI@users.noreply.github.com>
2025-07-22 11:45:09 +0000

    added data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = 2025;
+let accountEmail = 'satyajeet@google.com';
+var accountPassword = '12345';
+accountCity = 'Inodre';
+
+// accountID = 2    // NOT ALLOWED
+
+console.log(accountID);
+
+/*
+ Prefer not to use var
+ bacause of issue in block scop and functional scop 
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+// number
+// bigInt
+// string = ""
+// boolean = ture/false
+// null = standalone value  [eg. let age = null;]
+// undefined = value not assign [eg. let username;]
+// symbol = unique
+
+// object 
+
+console.log(typeof);
